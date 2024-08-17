2653acc10075a8da4e55fd03ac4357af71bccaa8
jayagithub1998 <68367431+jayagithub1998@users.noreply.github.com>
2024-08-17 17:41:15 +0530

    Update fibonacci.java

--- day1/fibonacci.java
+++ day1/fibonacci.java
@@ -1,1 +1,20 @@
+package assignmentFibonacci;
+
+public class Fibonacci {
+	public static void main (String args[]) {
+		int first=0;
+		int second=1;
+		int next=0;
+		System.out.println("Fibonacci series until 1000");
+		for(int i=0;next<1000;i++) {
+			System.out.println(next);
+			first=second;
+			second=next;
+			next =first+second;
+			
+		}
+		
+	}
+
+}
 
