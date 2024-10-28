df72a1f708a7c4b36c4fb3c8584dbb739cd6f24e
jayagithub1998 <68367431+jayagithub1998@users.noreply.github.com>
2024-10-28 22:42:59 +0530

    Add files via upload

---
+++ Actuator_28102024/src/main/java/com/example/Actuator_28102024/Actuator28102024Application.java
@@ -0,0 +1,13 @@
+package com.example.Actuator_28102024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actuator28102024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actuator28102024Application.class, args);
+	}
+
+}
